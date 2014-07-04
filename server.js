dba14eb88719eddfbbaf0cc2dc1e34fe9cb0c9a6
Logan Rogers <loganrogerst@gmail.com>
2014-07-03 19:06:58 -0600

    fixing static files

--- server.js
+++ server.js
@@ -6,8 +6,6 @@ app.use(express.static(__dirname + '/static'));
 
 app.use(logfmt.requestLogger());
 
-console.log("test");
-
 var port = Number(process.env.PORT || 5000);
 app.listen(port, function() {
   console.log("Listening on " + port);
