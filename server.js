33d05561e4852359fa6b365c19c90dc72bd3b0fb
Logan Rogers <loganrogerst@gmail.com>
2014-07-03 19:02:26 -0600

    static files

--- server.js
+++ server.js
@@ -2,13 +2,11 @@
 var express = require("express");
 var logfmt = require("logfmt");
 var app = express();
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/static'));
 
 app.use(logfmt.requestLogger());
 
-app.get('/', function(req, res) {
-  res.send('Hello World!');
-});
+console.log("test");
 
 var port = Number(process.env.PORT || 5000);
 app.listen(port, function() {
